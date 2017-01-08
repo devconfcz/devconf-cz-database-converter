a83f6ede5caaa64b5cc50844689382cf05d4d8ae
Daniel Passos <daniel@passos.me>
2017-01-08 11:53:03 -0200

    Update csv source file name

--- src/main/java/cz/devconf/App.java
+++ src/main/java/cz/devconf/App.java
@@ -8,9 +8,9 @@ public class App {
 
     public static void main(String[] args) throws IOException {
         new Converter()
-                .readSpeakersFrom("/Users/passos/Downloads/speakers.csv")
-                .readSessionsFrom("/Users/passos/Downloads/sessions.csv")
-                .readSessionsDetailsFrom("/Users/passos/Downloads/sessions2.csv")
+                .readSpeakersFrom("/Users/passos/Downloads/devconfcz-speakers.csv")
+                .readSessionsFrom("/Users/passos/Downloads/devconfcz-sessions.csv")
+                .readSessionsDetailsFrom("/Users/passos/Downloads/devconfcz-sessions2.csv")
                 .convertAndWriteIn("/Users/passos/Desktop/devconf.json");
     }
 
